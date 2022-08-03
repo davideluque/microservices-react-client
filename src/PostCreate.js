be3fab1b512de7603ba990869f8e580c6a06ac34
David Cabeza <cabezadavide@gmail.com>
2022-08-03 22:34:08 +0200

    Adds and displays post creeate comment

---
+++ src/PostCreate.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const PostCreate = () => {
+  return <div>
+    <h1>Create a new post</h1>
+    <form>
+      <div className="form-group">
+      <label>Title:</label>
+      <input type="text" className="form-control" id="title" placeholder="Enter title" />
+      </div>
+      <button className="btn btn-primary">Submit</button>
+    </form>
+  </div>;
+};
+
+export default PostCreate;
