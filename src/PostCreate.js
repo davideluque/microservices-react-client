7973f36c6c934a1ecc5d77802215cfbe4e255780
David Cabeza <cabezadavide@gmail.com>
2022-08-03 22:54:21 +0200

    Create Post Implementation

--- src/PostCreate.js
+++ src/PostCreate.js
@@ -1,18 +1,34 @@
-import React from 'react';
+import React, { useState } from "react";
+import axios from "axios";
 
 const PostCreate = () => {
+  const [title, setTitle] = useState("");
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    await axios.post("http://localhost:4000/posts", { title });
+    setTitle("");
+  };
+
   return (
     <div>
       <h1>Create a new post</h1>
-      <form>
+      <form onSubmit={onSubmit}>
         <div className="form-group">
           <label>Title</label>
-          <input type="text" className="form-control" id="title" placeholder="Enter title" />
+          <input
+            type="text"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            className="form-control"
+            id="title"
+            placeholder="Enter title"
+          />
         </div>
         <button className="btn btn-primary">Submit</button>
       </form>
     </div>
-  ); 
+  );
 };
 
 export default PostCreate;
