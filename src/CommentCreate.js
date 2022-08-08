b338c12e496ed198f9d05b3b10e7b7c07d175cdf
David Cabeza <cabezadavide@gmail.com>
2022-08-09 00:01:18 +0200

    Add request to comment creation

--- src/CommentCreate.js
+++ src/CommentCreate.js
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 
 const CommentCreate = ({ postId }) => {
+  const [content, setContent] = useState('');
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+  
+    await axios.post(`http://localhost:4001/posts/${postId}/comments`, { content });
+  
+    setContent('');
+  };
+
   return <div>
-    <form>
+    <form onSubmit={onSubmit}>
       <div className="form-group">
         <label htmlFor="exampleFormControlTextarea1">Leave a comment</label>
-        <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+        <textarea value={content} onChange={e => setContent(e.target.value)} className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
       </div>
       <button type="submit" className="btn btn-primary">Submit</button>
     </form>
