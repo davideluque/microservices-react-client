8ca761fd965cfa9949b6431bc5232298edd7870c
David Cabeza <cabezadavide@gmail.com>
2022-10-02 16:37:50 +0200

    Changes to comment list to show content based on status

--- src/CommentList.js
+++ src/CommentList.js
@@ -5,9 +5,23 @@ const CommentList = ({ comments }) => {
     <div>
       <h2>Comments</h2>
       <ul>
-        {comments.map(comment => (
-          <li key={comment.id}>{comment.content}</li>
-        ))}
+        {comments.map(comment => {
+          let content;
+
+          if (comment.status === 'approved') {
+            content = comment.content;
+          }
+
+          if (comment.status === 'pending') {
+            content = 'This comment is awaiting moderation';
+          }
+
+          if (comment.status === 'rejected') {
+            content = 'This comment has been rejected';
+          }
+
+          return <li key={comment.id}>{content}</li>
+        })}
       </ul>
     </div>
   );
